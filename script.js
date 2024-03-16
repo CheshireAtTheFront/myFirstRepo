8c335b4058f31dc3c378e4646de4aac4c8679747
Diana Makarova <din_m-v@mail.ru>
2024-03-16 14:50:25 +0300

    добавление и изменение DOM сайта (серия книг)

--- script.js
+++ script.js
@@ -1,141 +1,44 @@
-"use strict";
+// prepend(в нутри блока) и before(за блоком) - до
+// append(в нутри блока) и after(за блоком) - после
 
-const appData = {
-  title: "",
-  screens: [], // тип экрана
-  screenPrice: 0, // цена верстки
-  adaptive: true,
-  services: {}, // услуги
-  fullPrice: 0, // итоговая стоимость
-  servicePercentPrice: 0,  // процентная стоимость услуги
-  allServicePrices: 0, // все цены на услуги
-  rollback: 66, // откат
-  // метод функционала
-  start: function () {
-    this.getTitle();
-    this.asking();
-		this.priceOfAdditionalServices();
-		this.questionAddaptiv();
-		this.addPrices();
-    this.getFullPrise();
-    this.getServicePercentPrices();
-    this.logger();
-  },
-	// название проекта
-	getTitle: function () {
-		do {
-			this.title = prompt("Как называется ваш проект?", "  каЛькуляТор");
-		} while (!this.isString(this.title))
+// взята все блоки книг
+let blokbooks = document.querySelectorAll('.book');
+// взят body
+let background = document.querySelector('body');
+// взята ссылка-заголовок третьей книги
+let titleLink = blokbooks[4].querySelector('a');
+// взят блок рекламы
+let adv = document.querySelector('.adv');
+// взят весь список 2-ой книги
+let listSecondBook = blokbooks[0].querySelectorAll('li');
+// взят весь список 5-ой книги
+let listFifthBook = blokbooks[5].querySelectorAll('li');
+// взят весь список 6-ой книги
+let listSixthBook = blokbooks[2].querySelectorAll('li');
+// в 6-ой книге добавлен элемент li
+let newElemSixthBook = document.createElement('li');
 
-		this.changeTitle(this.title);
 
-	},
-// вопросы по стоимости работы
-  asking: function () {
-			for (let i = 0; i < 2; i++) {
-				let name = "";
-				let price = 0;
-
-				do {
-					name = prompt("Какие типы экранов нужно разработать?", "Простые");
-				} while (!this.isString(name))
-
-				do {
-					price = prompt("Сколько будет стоить данная работа?", 25000);
-				} while (!this.isNumber(price))
-
-				this.screens.push({id: i, name: name, price: +price});
-
-			}
-
-  },
-// вопрос о цене на доп услуги
-	priceOfAdditionalServices: function () {
-		for (let i = 0; i < 2; i++) {
-			let name = "";
-			let price = 0;
-	
-			do {
-				name = prompt("Какой дополнительный тип услуги нужен?", "метрика");
-			} while (!this.isString(name))
-	
-			do {
-				price = prompt("Сколько это будет стоить?"); // стоимость услуг
-			} while (!this.isNumber(price))  
-
-			this.services[name + [i]] = +price;
-	
-			}
-	},
-	// вопрос по адаптиву
-	questionAddaptiv: function () {
-		this.adaptive = confirm("Нужен ли адаптив на сайте?");
-	},
-	// сумма экранов и доп. услуг
-	addPrices: function () {
-		for (let key in this.services) {
-			this.allServicePrices += this.services[key];
-		} 
-
-		let result = this.screens.reduce(function (screenPrice, screen) {
-			return screenPrice + +screen.price
-		}, 0)
-
-		this.screenPrice = result;
-
-		// for (let screen of this.screens) {
-		// 	this.screenPrice += +screen.price
-		// }
-	},
-
-  // редактирование название проекта
-  changeTitle: function () {
-    this.title = this.title.trim()[0].toUpperCase() + this.title.trim().slice(1).toLowerCase();
-  },
-	// проверка на строку
-	isString: function (str) {
-		if (isNaN(str)) {
-			return true
-		} else {
-			return false;
-		}
-	},
-  // проверка на число
-  isNumber: function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-  },
-  // стоимость верстки + доп.услуг
-  getFullPrise: function () {
-    this.fullPrice = this.screenPrice + this.allServicePrices
-  },
-  // итоговая стоимость за вычетом процента отката
-  getServicePercentPrices: function () {
-    this.servicePercentPrice = Math.ceil(this.fullPrice - (this.fullPrice *(this.rollback / 100)));
-  },
-  // определение скидки
-  getRollbackMassege: function (price) {
-    if (price >= 30000) {
-      return "Даем скидку в 10%";
-    } else if (price >= 15000 && price < 30000) {
-      return "Даем скидку в 5%";
-    } else if (price < 15000 && price >= 0) {
-      return "Скидка не предусмотрена";
-    } else {
-      return "Что то пошло не так";
-    }
-  },
-  // метод вывода в консоль
-  logger: function () {
-    for (let key in appData) {
-      console.log("Ключ: " + key + " " + "Значение: " + appData[key]);
-    }
-		console.log(this.fullPrice);
-		console.log(this.servicePercentPrice);
-		console.log(this.allServicePrices);
-		console.log(this.screens);
-		console.log(this.services);
-		console.log(this.screenPrice);
-  },
-};
-
-appData.start();
+// Заменить картинку заднего фона на другую из папки image
+background.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
+// Исправить заголовок в книге 3
+titleLink.textContent = "Книга 3. this и Прототипы Объектов";
+// добавить текст для элемент li в шетсой книге и поместить его на свое место
+newElemSixthBook.innerHTML = 'Глава 8: За пределами ES6';
+listSixthBook[8].after(newElemSixthBook);
+// Удалить рекламу со страницы
+adv.remove();
+// Востановить порядок блока книг
+blokbooks[0].before(blokbooks[1]);
+blokbooks[0].after(blokbooks[4]);
+blokbooks[5].after(blokbooks[2]);
+// востановить порядок списка второй книги
+listSecondBook[2].before(listSecondBook[3]);
+listSecondBook[3].after(listSecondBook[6]);
+listSecondBook[4].before(listSecondBook[8]);
+listSecondBook[9].after(listSecondBook[2]);
+// востановить порядок списка пятой книги
+listFifthBook[3].after(listFifthBook[2]);
+listFifthBook[3].before(listFifthBook[9]);
+listFifthBook[5].after(listFifthBook[2]);
+listFifthBook[7].after(listFifthBook[5]);
